f49b7c11f78ecd48906bace6f4eca480b04f9707
Jared Siemen <siemenjm@gmail.com>
2022-11-30 16:38:15 -0500

    Use CSS media queries for responsiveness

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
-import MediaQuery from 'react-responsive';
 import { FaBars, FaRegWindowClose } from 'react-icons/fa';
 import siteLogo from '../public/images/generic_logo.png';
 import style from '../styles/Header.module.css';
+import { useState } from "react";
 
 export default function Header() {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    function handleClick() {
+        setToggleMenu(true);
+    }
+
     return (
         <div className={style.header}>
             <Image
@@ -15,33 +21,18 @@ export default function Header() {
                 width={130}
             />
             <nav>
-                <MediaQuery minWidth={1025}>
-                    <ul className={`${style.navList} ${style.desktop}`}>
-                        <li>
-                            <Link href='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link href='/about'>About</Link>
-                        </li>
-                        <li>
-                            <Link href='/contact'>Contact</Link>
-                        </li>
-                    </ul>
-                </MediaQuery>
-                <MediaQuery maxWidth={1024}>
-                    <FaBars className={style.icon} />
-                    <ul className={`${style.navList} ${style.tablet}`}>
-                        <li>
-                            <Link href='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link href='/about'>About</Link>
-                        </li>
-                        <li>
-                            <Link href='/contact'>Contact</Link>
-                        </li>
-                    </ul>
-                </MediaQuery>
+                <ul className={`${style.navList} ${style.desktop}`}>
+                    <li>
+                        <Link href='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link href='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link href='/contact'>Contact</Link>
+                    </li>
+                </ul>
+                <FaBars className={`${style.icon} ${style.tablet}`} />
             </nav>
         </div>
     )
