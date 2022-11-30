3caa8f450c3a4a3893df77373a1ce747591d4dea
Jared Siemen <siemenjm@gmail.com>
2022-11-30 14:54:53 -0500

    Add basic css styles to Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,30 @@
-
+import Image from "next/image";
+import Link from "next/link";
+import siteLogo from '../public/images/generic_logo.png';
+import style from '../styles/Header.module.css';
 
 export default function Header() {
     return (
-        <div>Header</div>
+        <div className={style.header}>
+            <Image
+                priority
+                src={siteLogo}
+                alt='site logo'
+                width={130}
+            />
+            <nav>
+                <ul className={style.navList}>
+                    <li>
+                        <Link href='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link href='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link href='/contact'>Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
