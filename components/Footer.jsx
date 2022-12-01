8ca262456ee8b4dcef238c168a4b309246f36220
Jared Siemen <siemenjm@gmail.com>
2022-12-01 15:39:08 -0500

    Add basic Footer and styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,18 @@
+import styles from '../styles/Footer.module.css';
+
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <footer className={styles.footer}>
+            <div className={styles.upperFooter}>
+                <div className={`${styles.innerContainer} ${styles.leftContainer}`}>Contact Info</div>
+                <div className={`${styles.innerContainer} ${styles.centerContainer}`}>Nav Menu</div>
+                <div className={`${styles.innerContainer} ${styles.rightContainer}`}>Logo</div>
+            </div>
+            <div className={styles.lowerFooter}>
+                <div className={`${styles.innerContainer} ${styles.leftContainer}`}>Copyright</div>
+                <div className={`${styles.innerContainer} ${styles.centerContainer}`}>Privacy Policy</div>
+                <div className={`${styles.innerContainer} ${styles.rightContainer}`}>Developer/Designer</div>
+            </div>
+        </footer>
     );
 }
