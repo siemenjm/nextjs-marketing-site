6bb4237abc6c6281184dde31f3a2016493973c95
Jared Siemen <siemenjm@gmail.com>
2022-11-30 20:10:56 -0500

    Add transition to tablet nav menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { FaBars, FaRegWindowClose } from 'react-icons/fa';
 import siteLogo from '../public/images/generic_logo.png';
 import style from '../styles/Header.module.css';
-import { useState } from "react";
 
 export default function Header() {
     const [toggleMenu, setToggleMenu] = useState(false);
@@ -11,6 +11,9 @@ export default function Header() {
     function handleClick() {
         toggleMenu ? setToggleMenu(false) : setToggleMenu(true);
     }
+
+    let transition;
+    toggleMenu ? transition = style.transition : transition = ''; 
 
     return (
         <div className={style.header}>
@@ -25,6 +28,11 @@ export default function Header() {
             <nav>
                 <ul className={`${style.navList} ${style.desktop}`}>
                     <li>
+                        <div className={`${style.iconContainer} ${style.shiftRight}`}>
+                            <FaRegWindowClose onClick={handleClick} className={`${style.icon} ${style.tablet}`} />
+                        </div>
+                    </li>
+                    <li>
                         <Link href='/'>Home</Link>
                     </li>
                     <li>
@@ -35,23 +43,24 @@ export default function Header() {
                     </li>
                 </ul>
 
+                <ul className={`${style.navList} ${style.tablet} ${transition}`}>
+                    <li>
+                        <div className={`${style.iconContainer} ${style.shiftRight}`}>
+                            <FaRegWindowClose onClick={handleClick} className={`${style.icon} ${style.tablet}`} />
+                        </div>
+                    </li>
+                    <li>
+                        <Link href='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link href='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link href='/contact'>Contact</Link>
+                    </li>
+                </ul>
                 {toggleMenu ?
-                    <ul className={`${style.navList} ${style.tablet}`}>
-                        <li>
-                            <div className={`${style.iconContainer} ${style.shiftRight}`}>
-                                <FaRegWindowClose onClick={handleClick} className={`${style.icon} ${style.tablet}`} />
-                            </div>
-                        </li>
-                        <li>
-                            <Link href='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link href='/about'>About</Link>
-                        </li>
-                        <li>
-                            <Link href='/contact'>Contact</Link>
-                        </li>
-                    </ul>
+                    <></> 
                 :
                     <div className={`${style.iconContainer}`}>
                         <FaBars onClick={handleClick} className={`${style.icon} ${style.tablet}`} />
